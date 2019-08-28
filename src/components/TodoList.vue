ba52917c28d73c751ffe8347234b19b658e2f9d8
Hemanshu Khodiyar <hemanshu.eng@gmail.com>
2019-08-28 17:50:47 +0100

    add todo list complete

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,9 +1,18 @@
 <template>
   <div>
-    <input type="text" class="todo-input" placeholder="What needs to be done" v-model="newTodo"
-    @keyup.enter="addTodo" />
-    <div v-for="todo in todos" :key="todo.id" class="toso-item">
+    <input
+      type="text"
+      class="todo-input"
+      placeholder="What needs to be done"
+      v-model="newTodo"
+      @keyup.enter="addTodo"
+    />
+    <div v-for="(todo,index) in todos" :key="todo.id" class="todo-item">
       {{todo.title}}
+      <div class="remove-item"
+       @click="removeTodo(index)"
+      >&times;
+      </div>
     </div>
   </div>
 </template>
@@ -13,35 +22,40 @@ export default {
   name: "todo-list",
   data() {
     return {
-      newTodo:'',
-      idForTodo:3,
-      todos:[
+      newTodo: "",
+      idForTodo: 3,
+      todos: [
         {
-          'id':1,
-          'title':'Finish Vue todoapp',
-          'completed':false,
+          id: 1,
+          title: "Finish Vue todoapp",
+          completed: false
         },
         {
-          'id':2,
-          'title':'Create app with laravel',
-          'completed':false,
-        },
+          id: 2,
+          title: "Create app with laravel",
+          completed: false
+        }
       ]
     };
   },
-  methods:{
-    addTodo(){
+  methods: {
+    addTodo() {
+      if (this.newTodo.trim().length == 0) {
+        return;
+      }
       this.todos.push({
-        id:this.idForTodo,
-        title:this.newTodo,
-        completed:false,
-      })
-      this.newTodo =''
-      this.idForTodo++ 
+        id: this.idForTodo,
+        title: this.newTodo,
+        completed: false
+      });
+      this.newTodo = "";
+      this.idForTodo++;
+    },
+    removeTodo(index){
+      this.todos.splice(index,1)
     }
-  
-}
-}
+  }
+};
 </script>
 
 <style lang="scss">
@@ -52,6 +66,21 @@ export default {
   margin-top: 16px;
   &:focus {
     outline: none;
+  }
+  margin-bottom: 15px;
+}
+.todo-item {
+  margin-bottom: 12px;
+  padding: 5px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+.remove-item {
+  cursor: pointer;
+  margin-left: 14px;
+  &:hover {
+    color: black;
   }
 }
 </style>
