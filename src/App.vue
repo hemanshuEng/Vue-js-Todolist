bb3cef1e2f952b66c34935f512f6b9d30ed0ab3c
Hemanshu Khodiyar <hemanshu.eng@gmail.com>
2019-08-28 21:46:01 +0100

    updated

--- src/App.vue
+++ src/App.vue
@@ -6,12 +6,11 @@
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+
 import TodoList from "./components/TodoList.vue";
 export default {
   name: "app",
   components: {
-    HelloWorld,
     TodoList
   }
 };
